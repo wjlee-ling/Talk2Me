49f9c572de6539fe32f6b86312821d89ebab9342
wjlee-ling <wjlee.ling@gmail.com>
2023-04-28 18:01:50 +0900

    [fix] applied black formatting

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ import streamlit as st
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 engine = st.secrets.GPT_MODEL
 messages: typing.List[dict] = [
-        {"role": "system", "content": "Let's play role-play. Remember that I'm a Korean learning English and you are a fluent English speaker. Let's say you work at a coffee shop and I am a client."},
-        {"role": "user", "content": "Hi"},
+    {
+        "role": "system",
+        "content": "Let's play role-play. Remember that I'm a Korean learning English and you are a fluent English speaker. Let's say you work at a coffee shop and I am a client.",
+    },
+    {"role": "user", "content": "Hi"},
 ]
 
-st.title("Learn English") 
+st.title("Learn English")
+
 
 def input_prmpt(messages: typing.List[dict]) -> str:
     completion = openai.ChatCompletion.create(
@@ -26,6 +30,7 @@ def input_prmpt(messages: typing.List[dict]) -> str:
     print("================")
     return completion.choices[0].message.content
 
+
 # Define a function to prompt the user for input and generate a response
 def chat():
     turns = 0
@@ -36,16 +41,20 @@ def chat():
         if turns == 7:
             break
         turns += 1
-        messages.append({
-            "role" : "user",
-            "content" : prompt,
-        })
+        messages.append(
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        )
         response = input_prmpt(messages)
         st.write("Bot: ", response)
-        messages.append({
-            "role": "assistant",
-            "content": response,
-        })
+        messages.append(
+            {
+                "role": "assistant",
+                "content": response,
+            }
+        )
 
         prompt = st.text_input(label="You: ", key=turns)
         st.write(f"You: {prompt}")
@@ -54,14 +63,16 @@ def chat():
         correction = correct(messages)
         st.write(f"Here is my advice for you:\n{correction}")
 
+
 def correct(messages):
     correction_request = {
         "role": "assistant",
-        "content": "Could you give me feedback about my English in Korean?"
+        "content": "Could you give me feedback about my English in Korean?",
     }
     messages.append(correction_request)
     response = input_prmpt(messages)
     return response
+
 
 # Run the chatbot with a specific model
 chat()
