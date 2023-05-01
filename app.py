00b6c047a454051abd7429613db0d3d1bf58acf3
wjlee-ling <wjlee.ling@gmail.com>
2023-05-01 19:28:17 +0900

    [Feat] create history.pickle

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import typing
+import pickle
 import openai
 import streamlit as st
 
@@ -23,6 +24,17 @@ proficiency = st.sidebar.radio(
     options=["Beginner", "Intermediate", "Advanced"],
 )
 st.write(f"We're talking in {st.session_state.language} at the level of {st.session_state.proficiency}.")
+if "history" not in st.session_state : # ❗️TO-DO: file name w/ user unique ID
+    with open("history.pickle", "wb") as f:
+        st.session_state.history: typing.List[dict] = [
+        {
+            "role": "system",
+            "content": f"Let's play role-play. Remember that I'm a Korean learning {st.session_state.language} and you are a fluent {st.session_state.language} speaker. My proficieny of {st.session_state.language} is {st.session_state.proficiency}. Let's say you work at a coffee shop and I am a client.",
+        },
+        {"role": "user", "content": "Hi"},
+        ]
+        pickle.dump(st.session_state.history, f)
+
 if "dialogue" not in st.session_state:
     st.session_state.dialogue = ""
 st.write(st.session_state.dialogue)
