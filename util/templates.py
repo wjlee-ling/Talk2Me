346783b79bd3e37d9894496527ea365c5ab12d0e
wjlee-ling <wjlee.ling@gmail.com>
2023-05-05 19:57:59 +0900

    [fix] del wav file as soon as get transcript

--- util/templates.py
+++ util/templates.py
@@ -13,31 +13,31 @@ def question_template(page_idx):
     # hide_pages(hide_idx)
     st.subheader(f"Q{page_idx}. {st.session_state.questions[page_idx]}")
 
-    path = Path(f"q{page_idx}.wav")
-    if True: # not path.exists():
-        wav_bytes = get_mic_input()
-        if wav_bytes:
-            if path.exists():
-                path.unlink()
-            with open(f"q{page_idx}.wav", mode="bx") as f:
-                f.write(wav_bytes)
-            audio = open(f"q{page_idx}.wav", "rb")
-            transcript = get_transcript(audio)
-            st.session_state.db.insert_one(
-                collection = "interviews",
-                insertion = {
-                "question" : st.session_state.db.questions[page_idx],
-                "answer" : transcript,
-                }
-            )
+    wav_bytes = get_mic_input()
+    if wav_bytes:
+        path = Path(f"q{page_idx}.wav")
+        if path.exists():
+            path.unlink()
+        with open(f"q{page_idx}.wav", mode="bx") as f:
+            f.write(wav_bytes)
+        audio = open(f"q{page_idx}.wav", "rb")
+        transcript = get_transcript(audio)
+        st.session_state.db.insert_one(
+            collection = "interviews",
+            insertion = {
+            "question" : st.session_state.questions[page_idx],
+            "answer" : transcript,
+            }
+        )
+        del wav_bytes
 
     doc = st.session_state.db.find_one(
         collection="interviews",
-        hint=st.session_state.questions[page_idx],
+        hint={"question":st.session_state.questions[page_idx]},
         )
-    
+
     if doc and "answer" in doc:
-        st.write(doc.answer)
+        st.write(doc["answer"])
         if page_idx != 2: 
             if st.button("next"):
                 switch_page(f"question{page_idx+1}")
