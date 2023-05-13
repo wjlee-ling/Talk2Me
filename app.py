cd888f819aced57c0b7174de320520a008487162
wjlee-ling <wjlee.ling@gmail.com>
2023-05-13 20:39:03 +0900

    [Fix] background_survey and get first question recordding-checked

--- app.py
+++ app.py
@@ -4,36 +4,43 @@ import openai
 import streamlit as st
 from collections import defaultdict
 from util.utils import *
-from util.templates import question_template
+from util.templates import AnswerTemplate, question_template, recording_template
 from util.query_database import init_database
 from streamlit_extras.switch_page_button import switch_page
 from st_pages import hide_pages
+
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 hide_pages(["Question1", "Question2", "Question3"])
 
 st.title("Talk")
-st.header("Background Survey")
-with st.form("setting"):
-    st.subheader("What do you do for fun?")
-    leisure = (
-        st.radio(
-            "Choose one of the following",
-            key="leisure",
-            options=["Watch movies", "Read books"],
+if "survey" not in st.session_state:
+    with st.form("setting"):
+        st.header("Background Survey")
+        st.subheader("What do you do for fun?")
+        st.session_state.leisure = (
+            st.radio(
+                "Choose one of the following",
+                options=["Watch movies", "Read books"],
+            )
+            .lower()
+            .replace(" ", "_")
         )
-        .lower()
-        .replace(" ", "_")
-    )
 
-    submitted = st.form_submit_button("Start Test")
+        submitted = st.form_submit_button("Start Test")
+        if submitted:
+            st.session_state.survey = True
 
-if submitted:
-    st.session_state.db = init_database(user_id="admin", theme=leisure, n_questions=3)
+elif st.session_state.survey:
+    st.session_state.db = init_database(user_id="admin", theme=st.session_state.leisure, n_questions=3)
 
     if "questions" not in st.session_state:
-        st.session_state.questions = ["dummy"] + st.session_state.db.get_interview_questions(leisure)[:st.session_state.db.n_questions]
-        st.session_state.answers = defaultdict(str)
+        st.session_state.questions = st.session_state.db.get_interview_questions(st.session_state.leisure)[:st.session_state.db.n_questions]
+        st.session_state.transcripts = [""] * 3
 
     if "questions" in st.session_state:
-        question_template(page_idx=1)
+        st.session_state.current_idx = 0
+        recording_template(0)
+        # st.session_state.current_idx = 2
+        # recording_template(2)
+        # question_template(page_idx=1)
