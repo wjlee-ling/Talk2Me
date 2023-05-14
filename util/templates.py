5c52cab41e81c47c637d59485322f0e8fe322f8b
wjlee-ling <wjlee.ling@gmail.com>
2023-05-14 11:19:32 +0900

    [update] log wav_bytes to DB

--- util/templates.py
+++ util/templates.py
@@ -1,12 +1,12 @@
 from util.chat import get_feedback
 from util.utils import *
 from util.speech_to_text import get_mic_input, get_transcript
+import typing
 import streamlit as st
-from datetime import datetime
 from streamlit import session_state as sst
 
 
-def qa_template(page_idx):
+def qa_template(page_idx: int):
     question_item = sst.questions[page_idx]
     question_content = question_item["question"]
     question_type = question_item["type"]
@@ -22,19 +22,20 @@ def qa_template(page_idx):
         audio = open(audio_file_name, "rb")
         sst["answers"][page_idx]["transcript"] = get_transcript(audio)
 
-        st.session_state.db.insert_one(
+        sst.db.insert_one(
             collection="interviews",
             insertion={
                 "question": question_content,
                 "type": question_type,
                 "answer": sst["answers"][page_idx]["transcript"],
                 "feedback": "",
+                "audio": wav_bytes,
             },
         )
         st.experimental_rerun()
 
     st.subheader(question_content)
-    doc = st.session_state.db.find_latest(
+    doc = sst.db.find_latest(
         collection="interviews",
         hint={
             "question": question_content,
@@ -43,6 +44,7 @@ def qa_template(page_idx):
     )
     wav_bytes = get_mic_input()
     if wav_bytes and doc is None:
+        # first recording
         _record(page_idx, wav_bytes)
 
     elif doc is not None and wav_bytes and wav_bytes != sst["answers"][page_idx]["wav"]:
@@ -50,7 +52,13 @@ def qa_template(page_idx):
         _record(page_idx, wav_bytes)
 
     elif doc is not None:
+        # display user answer
         st.write(doc["answer"])
         if st.button("Next", key=f"next_button_{sst.current_idx}"):
-            sst.current_idx = 2
-            st.experimental_rerun()
+            if sst.current_idx == sst.db.n_questions: # To-do : n_themes * n_questions
+                # redirect to feedback page
+                feedback_template(doc)
+
+            else:
+                sst.current_idx += 1
+                st.experimental_rerun()
